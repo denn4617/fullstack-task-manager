fa4b1e9719eb464079fd6cdd6b4fa70ad3135c5e
denn4617 <dkilia19@student.aau.dk>
2025-03-03 13:48:20 +0100

    Implement Login UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import TaskManager from "./components/TaskManager";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Login />} />
         <Route path="/tasks" element={<TaskManager />} />
       </Routes>
     </Router>
